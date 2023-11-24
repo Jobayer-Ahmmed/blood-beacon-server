45df3dbf9b692b6c34ace9331e1e6dbae907625c
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2023-11-25 01:05:13 +0600

    adding basic decorating  server structure& connect database with mongoose

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express"
+import "dotenv/config"
+import applyMiddleWare from "./middlewares/applyMiddleWare.js"
+import connectDB from "./db/connectDB.js"
+
+const app =express()
+const port = process.env.PORT || 5000
+
+applyMiddleWare(app)
+
+app.get("/health", (req, res)=>{
+    res.send("Blood Donation server is running")
+})
+
+
+
+
+
+
+app.all("*", (req, res, next)=>{
+    const myErr = new Error(`The request URL is invalid : [${req.url}]`)
+    myErr.status=404
+    next(myErr)
+})
+
+app.use((err, req, res, next)=>{
+    res.status(err.status || 500).json({message : err.message})
+})
+
+const main = async()=>{
+    await connectDB()
+    app.listen(port,()=>console.log(`The port ${port} is running`))
+}
+main()
---
+++ src/db/connectDB.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import "dotenv/config"
+
+const getConnectionString=()=>{
+    let connectionURI = process.env.MONGO_DB_CLOUD
+    connectionURI = connectionURI.replace("<username>", process.env.DB_USER)
+    connectionURI = connectionURI.replace("<password>", process.env.DB_PASS)
+    return connectionURI
+
+}
+
+const connectDB = async()=>{
+    const uri = getConnectionString()
+
+    await mongoose.connect(uri, {dbName:process.env.DB_NAME})
+    console.log("database connection successfull")
+}
+
+export default connectDB
